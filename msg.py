8f30e960e79908187651702ea5c691e7d7283dc8
sausame <bi_song@163.com>
2017-07-23 22:48:28 +0800

    Add message center

---
+++ msg.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+
+import codecs
+import io
+import os
+import random
+import re
+import urllib2
+
+import requests
+import datetime
+import itchat
+import time
+
+from itchat.content import TEXT
+from qwd import QWD
+
+class Message:
+
+    def __init__(self, qwd):
+        self.qwd = qwd
+
+    def translate(self, msg):
+
+        # print('------测试-----' + msg['Content'])
+        reg_url_cn = r'(http://url\.cn/\S\S\S\S\S\S\S)'
+        result = re.findall(reg_url_cn, msg)
+        # print(result)
+        if len(result)>0:
+            text = QWD.get_new_msg(msg, result)
+        else:
+            text = msg
+
+        # print(text)
+        https_result = re.findall(r'(https://union-click\.jd\.com/jdc\?d=\S\S\S\S\S\S)', text)
+        print text, https_result
+
+        if len(https_result) == 0:
+            return None
+
+        for i in range(len(https_result)):
+            self.skuid = QWD.get_skuid(str(https_result[i]))
+            skuurl = self.qwd.getShareUrl(self.skuid)
+            money_msg = text.replace(https_result[i], skuurl)
+            self.text = money_msg
+
+            url_img = self.qwd.get_img(self.skuid)
+
+            self.img = 'img/{0}.jpg'.format(self.skuid)
+            if not os.path.exists('img'):
+                os.mkdir('img')
+
+            with codecs.open(self.img, 'wb') as f:
+                f.write(urllib2.urlopen(url_img).read())
+
+            print self.skuid, self.text, self.img
+
+        return self
+
+class MessageCenter:
+
+    def __init__(self, configFile=None):
+
+        self.qwd = QWD()
+        self.qwd.login()
+
+    def translate(self, msg):
+
+        message = Message(self.qwd)
+        return message.translate(msg)
+
--- wx.py
+++ wx.py
@@ -13,7 +13,7 @@ import itchat
 import time
 
 from itchat.content import TEXT
-from qwd import QWD
+from msg import MessageCenter
 
 class WX:
 
@@ -29,11 +29,9 @@ class WX:
 
         self.toGroups = list()
 
-        self.qwd = QWD()
+        self.messageCenter = MessageCenter()
 
     def login(self):
-
-        self.qwd.login()
 
         itchat.auto_login(hotReload=True, statusStorageDir='status.pkl')
 
@@ -66,16 +64,11 @@ class WX:
 
     def text(self, msg):
 
-        def get_image(image_storage, url):
-            r = requests.get(url, stream=True)
-            for block in r.iter_content(1024):
-                image_storage.write(block)
-            image_storage.seek(0)
-
-        # print('FromUserName')
-        # print(msg['FromUserName'])
-        # print(msg['Content'])
+        #XXX: Move to message center
         print(self.set_skuid)
+
+        #XXX: Reset skuids every 3:00 am
+        #TODO: Bad design makes human confused
         now_time = datetime.datetime.now()
         reg_time = r'20\w\w-\w\w-\w\w 03:0[0-1]:00\S+'
         result_time = re.findall(reg_time, str(now_time))
@@ -84,79 +77,40 @@ class WX:
         text = ''
         if len(self.jk_id) != len(self.jk_name):
             self.jk_id = self.get_jkid()
-        # print('jk_id')
-        # print(jk_id)
-        # print('ng_id')
-        # print(ng_id)
+
         if len(self.ng_id) != len(self.ng_name):
             self.ng_id = self.get_ng_id()
-        # print('ng_id')
-        # print(ng_id)
 
         print msg['Content']
 
         for i in range(len(self.jk_id)):
+
             print i, self.jk_id[i], msg['FromUserName']
             if msg['FromUserName'] == self.jk_id[i]:
-                # print('------测试-----' + msg['Content'])
-                reg_url_cn = r'(http://url\.cn/\S\S\S\S\S\S\S)'
-                result = re.findall(reg_url_cn, msg['Content'])
-                # print(result)
-                if len(result)>0:
-                    text = QWD.get_new_msg(msg['Content'], result)
-                else:
-                    text = msg['Content']
-                # print(text)
-                https_result =  re.findall(r'(https://union-click\.jd\.com/jdc\?d=\S\S\S\S\S\S)', text)
-                print text, https_result
-                if len(https_result)>0:
-                    for i in range(len(https_result)):
-                        skuid = QWD.get_skuid(str(https_result[i]))
-                        skuurl = self.qwd.getShareUrl(skuid)
-                        money_msg = text.replace(https_result[i], skuurl)
-                        text = money_msg
-                        url_img = self.qwd.get_img(skuid)
 
-                        # XXX: Directly send image buffer
-                        '''
-                        image_storage = io.BytesIO()
-                        get_image(image_storage, url_img)
-                        '''
-                        img = 'img/{0}.jpg'.format(skuid)
-                        if not os.path.exists('img'):
-                            os.mkdir('img')
+                message = self.messageCenter.translate(msg['Content'])
 
-                        with codecs.open(img, 'wb') as f:
-                            f.write(urllib2.urlopen(url_img).read())
+                if message is None:
+                    continue
 
-                        print skuid, skuurl, text, url_img, img
+                # XXX: Move to message center
+                if message.skuid in list(self.set_skuid):
+                    continue
 
-                    if skuid not in list(self.set_skuid):
+                self.set_skuid.add(message.skuid)
 
-                        self.set_skuid.add(skuid)
+                for group in self.toGroups:
 
-                        for group in self.toGroups:
+                    interval = random.random() * 10
+                    time.sleep(interval)
 
-                            interval = int(random.random() * 5)
-                            print interval
+                    ret = group.send(message.text)
+                    print 'Send', message.text, ':', ret
 
-                            time.sleep(interval)
-                            #itchat.send(text, toUserName=ng_id[j])
-                            ret = group.send(text)
-                            print ret, text
+                    interval = random.random() * 10
+                    time.sleep(interval)
 
-                            time.sleep(interval)
+                    ret = group.send_image(message.img)
+                    print 'Send', message.img, ':', ret
 
-                            # XXX: Directly send image buffer
-                            #ret = group.send_image(image_storage)
-                            ret = group.send_image(img)
-                            print ret
-                            # if len(aaaa['BaseResponse']['ErrMsg']) == 0:
-                            #      itchat.send("@img@%s".format(img))
-
-                    # XXX: Directly send image buffer
-                    #image_storage.close()
-
-                        # print(int(random.random() * 500))
-                        # time.sleep(int(random.random() * 500))
 
